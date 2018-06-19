10d16e00af81a5e5cdea074923d54d35487ef79d
Administrator@cloudsjmteam.co.in <Administrator@cloudsjmteam.co.in>
2018-06-19 12:09:46 +0530

    19th July - seccond commit

--- connection.tf
+++ connection.tf
@@ -1,17 +1,16 @@
 provider "google" {
-    credentials = "${file("C:/repo/account.json")}"
-	project = "innate-gizmo-187004"
-	region = "asia-south1"
+  credentials = "${file("C:/repo/account.json")}"
+  project     = "innate-gizmo-187004"
+  region      = "asia-south1"
 }
 
 provider "aws" {
-    region = "ap-south-1"
+  region = "ap-south-1"
 }
 
 provider "azurerm" {
-    subscription_id ="0"
-	client_id = "1"
-    client_secret = "2"
-    tenant_id ="3"
+  subscription_id = "0"
+  client_id       = "1"
+  client_secret   = "2"
+  tenant_id       = "3"
 }
- 	
--- resource.tf
+++ resource.tf
@@ -1,13 +1,27 @@
 resource "google_compute_network" "our_dev" {
-    name = "devnet"
-	auto_create_subnetworks = true
+  name                    = "devnet"
+  auto_create_subnetworks = true
 }
 
 resource "aws_vpc" "example_vpc" {
-    cidr_block = "10.0.0.0/16"
-	enable_dns_hostnames = true
-	enable_dns_support = true
-	tags {
-	    Name = "Terrafor_VPC"
-	}
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags {
+    Name = "Terrafor_VPC"
+  }
 }
+
+resource "aws_subnet" "subnet1" {
+  cidr_block        = "${cidrsubnet(aws_vpc.example_vpc.cidr_block, 3, 1)}"
+  vpc_id            = "${aws_vpc.example_vpc.id}"
+  availability_zone = "ap-south-1a"
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block        = "${cidrsubnet(aws_vpc.example_vpc.cidr_block, 2, 2)}"
+  vpc_id            = "${aws_vpc.example_vpc.id}"
+  availability_zone = "ap-south-1b"
+}
+
